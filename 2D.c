7fff6b3c538256b8d89231e68fcfdcb7d8f2119e
Resbi <resbi@redstone.link>
2018-02-04 11:36:48 +0000

    Update 2D.c

--- 2D.c
+++ 2D.c
@@ -5,7 +5,7 @@ void main(){
   system("clear");
   printf("\033[99m"); //Set color.
   int H = 30;
-  int W = 60;
+  int W = 30;
   int Count = 0;
   int AX, AY, POINT;
   int Gird[H][W];
@@ -14,8 +14,8 @@ void main(){
   int i, j, k, l, a, b; //Set values for "for".
   
   srand(time(NULL));
-  for(a = 0; a < H; a++){
-    for(b = 0; b < W; b++){
+  for (a = 0; a < H; a++) {
+    for (b = 0; b < W; b++) {
       Gird[a][b] = rand() % 2; //Generate random point.
     }
   }
@@ -30,16 +30,16 @@ void main(){
     }
   }
   */
-  while(1){
-    printf("\x1b[0;0H");
+  while (1) {
+    printf ("\x1b[0;0H");
     //Generate new map.
-    for(a = 0; a < H; a++){
-      for(b = 0; b < W; b++){
+    for (a = 0; a < H; a++) {
+      for (b = 0; b < W; b++) {
 	//Get neighbour.
 	Count = 0;
-	for(i = -1; i < 2; i++){
+	for (i = -1; i < 2; i++) {
 	  AY = (a + i + H) % H;
-	  for(j = -1; j < 2; j ++){
+	  for (j = -1; j < 2; j ++) {
 	    AX = (b + j + W) % W;
 	    Count += Gird[AY][AX];
 	  }
@@ -47,23 +47,23 @@ void main(){
 	Count -= Gird[a][b];
 	//Get new point.
 	POINT = Gird[a][b];
-	if(POINT == 1 && (Count < 2 || Count > 3)){
+	if (POINT == 1 && (Count < 2 || Count > 3)) {
 	  Temp[a][b] = 0;
-	}else if(POINT == 0 && Count == 3){
+	} else if (POINT == 0 && Count == 3){
 	  Temp[a][b] = 1;
-	}else{
+	} else {
 	  Temp[a][b] = POINT;
 	}
       }
     }
     //Flush the map then draw to screen and empty the temp.
-    for(a = 0; a < H; a++){
-      for(b = 0; b < W; b++){
+    for (a = 0; a < H; a++) {
+      for (b = 0; b < W; b++) {
 	Gird[a][b] = Temp[a][b];
 	Temp[a][b] = 0;
-	if(Gird[a][b]){
+	if (Gird[a][b]) {
 	  printf("# ");
-        }else{
+        } else {
 	  printf("  ");
 	}
       }
